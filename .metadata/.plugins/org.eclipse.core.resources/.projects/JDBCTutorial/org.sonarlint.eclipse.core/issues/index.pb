6242b6a00d9a94763fe3b921ae969144227f140a
gaudneha <neha.ev19@nsut.ac.in>
2021-08-12 17:07:20 +0530

    Initial Commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/JDBCTutorial/org.sonarlint.eclipse.core/issues/index.pb
@@ -0,0 +1,9 @@
+
+X
+(src/com/telstra/jdbc/JDBCInsertDemo.java,8\6\8620d52d8335aba88668d3366f70c3570721b38b
+X
+(src/com/telstra/jdbc/JdbcUpdateDemo.java,7\3\7325fe0f4841d77cf4234f9a96a36a7b0d5d4eeb
+X
+(src/com/telstra/jdbc/JdbcDeleteDemo.java,5\2\52537c94965a7315ef5893aa419c2d883b312df3
+X
+(src/com/telstra/jdbc/JdbcSelectDemo.java,4\a\4ac8d88c6c4d9bcc5a7afaf4f548cea4e23d3112
---
+++ JDBCTutorial/src/com/telstra/jdbc/JDBCInsertDemo.java
@@ -0,0 +1,40 @@
+package com.telstra.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCInsertDemo {
+	 public static void main(String[] args) {
+		
+		/* try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+	 
+	 String url = "jdbc:mysql://localhost:3306/telstra";
+	 String username = "root";
+	 String password = "password";
+	 String insertCommand ="INSERT INTO employee VALUE(8093,'Abhi','engineer')";
+	 int i = 0;
+	 Connection cn = null;
+	 Statement st = null;
+	 
+	 try {
+		cn = DriverManager.getConnection(url,username,password);
+	    st = cn.createStatement();
+	    i = st.executeUpdate(insertCommand);
+	    System.out.println(i + "Records added..");
+	 } catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	 
+	 
+	 
+	 }
+	
+}
